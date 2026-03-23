efbdddcd569627dad0512129b55a79366f7583a4
arc-jglee <jglee@arcsquare.ai>
2026-03-23 17:46:29 +0900

    Init: 프로젝트 초기화

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,44 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
-const eslintConfig = defineConfig([
+const tsconfigRootDir = process.cwd();
+
+export default defineConfig([
+  // Next 기본 규칙들
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
 
-export default eslintConfig;
+  // Prettier를 ESLint 안에서 돌리기
+  eslintPluginPrettierRecommended,
+
+  // 공통 ignore
+  globalIgnores([
+    '.next/**',
+    'out/**',
+    'build/**',
+    'public/**',
+    'next-env.d.ts',
+  ]),
+
+  {
+    files: ['src/**/*.{ts,tsx,mjs}'],
+    languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        tsconfigRootDir,
+      },
+    },
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'warn',
+      'simple-import-sort/exports': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-floating-promises': 'off',
+    },
+  },
+]);
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: 'export', // 정적 배포를 위해 output을 export로 설정합니다.
+  images: {
+    unoptimized: true, // (임시)정적 배포 시 Next.js 기본 이미지 최적화는 서버가 없어서 못 씁니다.
+  },
 };
 
 export default nextConfig;
